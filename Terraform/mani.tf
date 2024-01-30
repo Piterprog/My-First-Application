b7a63de8bb7db0a3a1b3b522353c9457384b19ad
Piter Kononihin <Piterbog@gmail.com>
2024-01-30 05:50:49 +0200

    Modif main

--- Terraform/mani.tf
+++ Terraform/mani.tf
@@ -19,12 +19,14 @@ terraform {
   }
 }
 
-#----------------------------------------------------------------------------------------
+#--------------------------------------VPC + IGW -----------------------------------------
 
 data "aws_availability_zones" "available" {}
 
 resource "aws_vpc" "main" {
-  cidr_block = var.vpc_cidr
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
   tags = {
     Name = "${var.env}-vpc"
   }
@@ -36,6 +38,7 @@ resource "aws_internet_gateway" "main" {
     Name = "${var.env}-igw"
   }
 }
+
 
 #--------------------------------------Public Subnets and Routing -------------------------
 

a95edbdcf5b8a6c2b556535aa1a2660fdcede95d
Piter Kononihin <Piterbog@gmail.com>
2024-02-02 04:01:02 +0200

    dd

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -4,8 +4,6 @@ terraform {
     bucket     = "vpc-piter-kononihin-terraform"
     key        = "dev/vpc/terraform.tfstate"
     region     = "us-east-1"
-    access_key = var.aws_access.key
-    secret_key = var.aws_secret_key
     encrypt    = true
   }
 }

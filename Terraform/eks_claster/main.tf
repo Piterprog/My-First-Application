bcd34d7ebf28d6270927283e856a7676f0b21328
Piter Kononihin <Piterbog@gmail.com>
2024-02-07 22:58:59 +0200

    dsdd

--- Terraform/eks_claster/main.tf
+++ Terraform/eks_claster/main.tf
@@ -36,20 +36,20 @@ data "aws_vpc" "existing_vpc" {
   id = data.terraform_remote_state.vpc.outputs.vpc_id
 }
 
-# resource "aws_iam_role" "eks_cluster_role" {
-#   name = "eks-cluster-role"
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "eks-cluster-role"
 
-#   assume_role_policy = jsonencode({
-#     Version   = "2012-10-17"
-#     Statement = [{
-#       Effect    = "Allow"
-#       Principal = {
-#         Service = "eks.amazonaws.com"
-#       }
-#       Action    = "sts:AssumeRole"
-#     }]
-#   })
-# }
+  assume_role_policy = jsonencode({
+    Version   = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+      Action    = "sts:AssumeRole"
+    }]
+  })
+}
 
 resource "aws_iam_policy_attachment" "eks_cluster_policy_attachment" {
   name       = "eks-cluster-policy-attachment"

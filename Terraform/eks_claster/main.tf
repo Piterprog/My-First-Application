9cb68b06d6943f0da991bbfedbffd01554df699e
Piter Kononihin <Piterbog@gmail.com>
2024-02-09 01:56:47 +0200

    eee

--- Terraform/eks_claster/main.tf
+++ Terraform/eks_claster/main.tf
@@ -59,7 +59,7 @@ resource "aws_eks_cluster" "my_cluster" {
 resource "aws_eks_node_group" "workers" {
   cluster_name    = aws_eks_cluster.my_cluster.name
   node_group_name = "workers"
-  node_role_arn   = aws_iam_role.eks_node_role.arn
+  node_role_arn   = aws_iam_role.node_instance_role.arn
 
   scaling_config {
     desired_size = 2

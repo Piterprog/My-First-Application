fc6924e39f2067ec08245896ac8dc02a3c50fbdc
Piter <152597719+Piterprog@users.noreply.github.com>
2024-02-09 16:31:10 +0200

    Update main.tf

--- Terraform/eks_claster/main.tf
+++ Terraform/eks_claster/main.tf
@@ -123,9 +123,7 @@ resource "aws_iam_role_policy_attachment" "eks_worker_node_policy_attachment" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
 
-resource "aws_iam_role_policy_attachment" "ecr_readonly_policy_attachment" {
-  role       = aws_iam_role.eks_node_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-}
+
+  
 
 

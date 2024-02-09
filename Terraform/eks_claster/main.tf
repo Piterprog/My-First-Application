b2117fa6f699c6186d0d87746c4c20baea299cbe
Piter Kononihin <Piterbog@gmail.com>
2024-02-10 01:10:29 +0200

    dsdad

--- Terraform/eks_claster/main.tf
+++ Terraform/eks_claster/main.tf
@@ -53,7 +53,7 @@ resource "aws_iam_role" "eks_cluster_role" {
 resource "aws_eks_cluster" "eks_cluster" {
   name     = "your_cluster_name"
   role_arn = aws_iam_role.eks_cluster_role.arn
-  version  = "1.27"
+  version  = "1.29"
 
   vpc_config {
     subnet_ids         = data.terraform_remote_state.vpc.outputs.private_subnet_ids 

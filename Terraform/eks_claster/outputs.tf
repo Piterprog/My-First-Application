9f56d965697dcfb33b5b3cc4396d62114293ec44
Piter Kononihin <Piterbog@gmail.com>
2024-02-09 23:52:31 +0200

    dasdasdd

--- Terraform/eks_claster/outputs.tf
+++ Terraform/eks_claster/outputs.tf
@@ -1,21 +1,20 @@
 
 output "cluster_name" {
-  value = aws_eks_cluster.my_cluster.name
+  value = aws_eks_cluster.eks_cluster.name
 }
 
 output "cluster_endpoint" {
-  value = aws_eks_cluster.my_cluster.endpoint
+  value = aws_eks_cluster.eks_cluster.endpoint
 }
 
 output "cluster_certificate_authority_data" {
-  value = aws_eks_cluster.my_cluster.certificate_authority.0.data
+  value = aws_eks_cluster.eks_cluster.certificate_authority.0.data
 }
 
 output "worker_group_id" {
-  value = aws_eks_node_group.workers.id
+  value = aws_eks_node_group.eks_nodes.id
 }
 
 output "worker_group_arn" {
-  value = aws_eks_node_group.workers.arn
+  value = aws_eks_node_group.eks_nodes.arn
 }
-

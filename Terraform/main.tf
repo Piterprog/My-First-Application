e8fcd07a0e1758a75e6c042c36efbda14c60a74b
Piter <152597719+Piterprog@users.noreply.github.com>
2024-02-11 12:32:28 +0200

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -157,26 +157,34 @@ resource "aws_security_group" "Security_vpc_Musad" {
   vpc_id      = aws_vpc.main.id 
 
   ingress {
-    description = "TLS from VPC"
-    from_port   = 443
-    to_port     = 443
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [var.Security_vpc_Musad]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    description = "HTTP from VPC"
+    description = "HTTP"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = [var.Security_vpc_Musad]
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+   ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = [var.Security_vpc_Musad]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {

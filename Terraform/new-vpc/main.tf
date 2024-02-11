2390b51120fd4b795a363297226f47fba3e3ddb7
Piter <152597719+Piterprog@users.noreply.github.com>
2024-02-11 10:44:25 +0200

    Update main.tf

--- Terraform/new-vpc/main.tf
+++ Terraform/new-vpc/main.tf
@@ -209,3 +209,48 @@ resource "aws_route" "private_route_b" {
   destination_cidr_block = "0.0.0.0/0"
   nat_gateway_id         = aws_nat_gateway.nat_gateway_b.id
 }
+
+
+#------------------------------------------ Security group ----------------------------------------------
+
+
+resource "aws_security_group" "security_mew_vpc" {
+  name        = "Security_vpc_new"
+  description = "Security group"
+  vpc_id      =  aws_vpc.main.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+   ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Security group new vpc"
+  }
+}

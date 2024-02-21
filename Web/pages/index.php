87847715cc9fe5e380a9b25da13a7015bd90c4ad
Piter Kononihin <Piterbog@gmail.com>
2024-02-21 06:09:55 +0200

    dsadasda

--- Web/pages/index.php
+++ Web/pages/index.php
@@ -1,18 +1,7 @@
 <?php
-    echo "<!DOCTYPE html>";
-    echo "<html lang=\"en\">";
-    echo "<head>";
-    echo "<meta charset=\"UTF-8\">";
-    echo "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">";
-    echo "<title>Welcome</title>";
-    echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"../styles/styles.css\">";
-    echo "</head>";
-    echo "<body>";
-    echo "<h1>Welcome</h1>";
-    echo "<p>Hello! Welcome to my page.</p>";
-    echo "<p>Your IP address is: " . $_SERVER['REMOTE_ADDR'] . "</p>";
-    echo "<p>Create by: Piter Kononihin...........</p>";
-    echo "<p>Hello! from kubernetes................</p>";
-    echo "</body>";
-    echo "</html>";
+$ip_server = $_SERVER['SERVER_ADDR'];
+
+echo "<h1>Hello From Kubernetes</h1><br>";
+echo "Server IP Address is: $ip_server", "<br><p>";
+echo "Made by <font color=blue>Denis Astahov";
 ?>

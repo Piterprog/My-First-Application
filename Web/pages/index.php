7b1544517dd280795e6c25ac22c05f99a59dda78
Piter Kononihin <Piterbog@gmail.com>
2024-02-21 05:22:56 +0200

    new file

---
+++ Web/pages/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+    <link rel="stylesheet" type="text/css" href="../styles/styles.css">
+</head>
+
+<body>
+    <h1>Welcome</h1>
+    <p>Hello! Welcome to my page.</p>
+    <p>Your IP address is: <?php echo $_SERVER['REMOTE_ADDR']; ?></p>
+    <p>Create by: Piter Kononihin.</p>
+    <p>Hello! from kubernetes.</p>
+</body>
+
+</html>

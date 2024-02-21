048c42f9cc29315782b5f9b6549e4fda68a56408
Piter Kononihin <Piterbog@gmail.com>
2024-02-21 06:06:39 +0200

    dadasda

--- Web/pages/index.php
+++ Web/pages/index.php
@@ -1,19 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome</title>
-    <link rel="stylesheet" type="text/css" href="../styles/styles.css">
-</head>
-
-<body>
-    <h1>Welcome</h1>
-    <p>Hello! Welcome to my page.</p>
-    <p>Your IP address is: <?php echo $_SERVER['REMOTE_ADDR']; ?></p>
-    <p>Create by: Piter Kononihin...........</p>
-    <p>Hello! from kubernetes................</p>
-</body>
-
-</html>
+<?php
+    echo "<!DOCTYPE html>";
+    echo "<html lang=\"en\">";
+    echo "<head>";
+    echo "<meta charset=\"UTF-8\">";
+    echo "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">";
+    echo "<title>Welcome</title>";
+    echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"../styles/styles.css\">";
+    echo "</head>";
+    echo "<body>";
+    echo "<h1>Welcome</h1>";
+    echo "<p>Hello! Welcome to my page.</p>";
+    echo "<p>Your IP address is: " . $_SERVER['REMOTE_ADDR'] . "</p>";
+    echo "<p>Create by: Piter Kononihin...........</p>";
+    echo "<p>Hello! from kubernetes................</p>";
+    echo "</body>";
+    echo "</html>";
+?>

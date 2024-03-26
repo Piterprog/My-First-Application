61cd29ec637b3b579eb3d38c13188cc2b99a3743
Piter <152597719+Piterprog@users.noreply.github.com>
2024-03-26 11:05:31 +0200

    Update 10.alb_and_tg.tf

--- terraform/10.alb_and_tg.tf
+++ terraform/10.alb_and_tg.tf
@@ -52,8 +52,21 @@ resource "aws_lb_listener" "https" {
     type             = "forward"
     target_group_arn = aws_lb_target_group.tg_web.arn
   }
+}
 
-  certificate_arn = "arn:aws:acm:us-east-1:381491829424:certificate/d0baa0ea-790a-46bb-98b3-aa31ebc56f99"  
+resource "aws_lb_listener" "http_redirect" {
+  load_balancer_arn = aws_lb.alb_web.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "redirect"
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
 }
 
 

104c03b05e276d5d829a9eaa26a2a6df0248031a
Piter <152597719+Piterprog@users.noreply.github.com>
2024-03-26 14:59:28 +0200

    Update 10.alb_and_tg.tf

--- terraform/10.alb_and_tg.tf
+++ terraform/10.alb_and_tg.tf
@@ -1,1 +1,62 @@
 
+
+resource "aws_lb" "alb_web" {
+  name               = "alb-web"
+  internal           = false
+  load_balancer_type = "application"
+  subnets            = ["subnet-081bf307e3e3c8d1b" ,"subnet-0e737729b771feef0"]
+  security_groups    = ["sg-0036443491e318478"]       
+
+  enable_deletion_protection = false
+
+  tags = {
+    Name = "alb_web"
+  }
+}
+
+resource "aws_lb_target_group" "tg_web" {
+  name        = "tg-web"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = "vpc-050567b109290b0fd"                  
+  target_type = "instance"
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    port                = "traffic-port"
+    interval            = 30
+    timeout             = 10
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.alb_web.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "redirect"
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.alb_web.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg_web.arn
+  }
+
+  certificate_arn = var.certificate_arn 
+}

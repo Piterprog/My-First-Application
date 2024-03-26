cab860d11c5047222938fb7ad20aa500dd6deaae
Piter <152597719+Piterprog@users.noreply.github.com>
2024-03-26 09:45:02 +0200

    Update 10.alb_and_tg.tf

--- terraform/10.alb_and_tg.tf
+++ terraform/10.alb_and_tg.tf
@@ -31,6 +31,17 @@ resource "aws_lb_target_group" "tg_web" {
   }
 }
 
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.alb_web.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg_web.arn
+  }
+}
+
 resource "aws_lb_listener" "https" {
   load_balancer_arn = aws_lb.alb_web.arn
   port              = "443"

2bc5901382713e723e117e4b1993201b44bb16f3
Piter <152597719+Piterprog@users.noreply.github.com>
2024-03-26 11:09:31 +0200

    Update 10.alb_and_tg.tf

--- terraform/10.alb_and_tg.tf
+++ terraform/10.alb_and_tg.tf
@@ -53,7 +53,7 @@ resource "aws_lb_listener" "https" {
     target_group_arn = aws_lb_target_group.tg_web.arn
   }
 
-  certificate_arn = "YOUR_CERTIFICATE_ARN_HERE"  # Замените на ARN вашего SSL/TLS сертификата
+  certificate_arn = "arn:aws:acm:us-east-1:381491829424:certificate/d0baa0ea-790a-46bb-98b3-aa31ebc56f99"
 }
 
 resource "aws_lb_listener" "http_redirect" {

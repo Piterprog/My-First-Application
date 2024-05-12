9233a0e9b37f660a99f717a34313b6f01fc51e0d
Piter Kononihin <Piterbog@gmail.com>
2024-05-13 00:53:30 +0300

    Change lenguage from RUS to ENG commits in file script.sh

--- bash-script/script.sh
+++ bash-script/script.sh
@@ -1,39 +1,39 @@
 #!/bin/bash
 
-# Проверяем, правильное ли количество аргументов передано
-if [ "$#" -ne 2 ]; then
-    echo "Использование: $0 {name} {env}"
+# Check if the correct number of arguments is provided
+if [ "$#" -ne 4 ]; then
+    echo "Usage: $0 {name} {env} {cluster_name} {region}"
     exit 1
 fi
 
-# Присваиваем входные данные переменным
+# Assign input variables
 name=$1
 env=$2
 cluster_name=$3
 region=$4
 
-# Получаем соответствующее определение задачи с последней ревизией
-task_definition=$(aws ecs list-task-definitions --family-prefix $name --sort DESC | jq -r '.taskDefinitionArns[0]')
+# Get the corresponding task definition with the latest revision
+task_definition=$(aws ecs list-task-definitions --family-prefix $name --sort DESC --region $region | jq -r '.taskDefinitionArns[0]')
 
-# Запускаем задачу в указанной среде
-result=$(aws ecs run-task --task-definition $task_definition --launch-type FARGATE --cluster $env)
+# Run the task in the specified environment
+result=$(aws ecs run-task --task-definition $task_definition --launch-type FARGATE --cluster $cluster_name --region $region)
 
-# Извлекаем идентификатор задачи
+# Extract the task ID
 task_id=$(echo $result | jq -r '.tasks[0].taskArn' | cut -d '/' -f 2)
 
-# Получаем статус задачи
-task_status=$(aws ecs describe-tasks --tasks $task_id --cluster $env | jq -r '.tasks[0].lastStatus')
+# Get the task status
+task_status=$(aws ecs describe-tasks --tasks $task_id --cluster $cluster_name --region $region | jq -r '.tasks[0].lastStatus')
 
-# Формируем ссылку на группу журналов
-log_group_link="https://console.aws.amazon.com/cloudwatch/home?region=<your-region>#logsV2:log-groups/log-group/"
+# Form the link to the log group
+log_group_link="https://console.aws.amazon.com/cloudwatch/home?region=$region#logsV2:log-groups/log-group/"
 
-# Выводим статус задачи и ссылку на группу журналов
+# Display the task status and log group link
 if [ "$task_status" == "RUNNING" ]; then
-    echo "Задача в данный момент выполняется."
-    echo "Ссылка на группу журналов: $log_group_link"
+    echo "The task is currently running."
+    echo "Log Group Link: $log_group_link"
 elif [ "$task_status" == "STOPPED" ]; then
-    echo "Задача остановлена."
-    echo "Ссылка на группу журналов: $log_group_link"
+    echo "The task has stopped."
+    echo "Log Group Link: $log_group_link"
 else
-    echo "Не удалось запустить задачу."
+    echo "Failed to start the task."
 fi

814b6c5346b1f146e82b0ee91ee55af9208b25fc
Piter <152597719+Piterprog@users.noreply.github.com>
2024-05-13 08:33:07 +0300

    Update script.sh

--- bash-script/script.sh
+++ bash-script/script.sh
@@ -1,31 +1,30 @@
 #!/bin/bash
 
 # Check if the correct number of arguments is provided
-if [ "$#" -ne 4 ]; then
-    echo "Usage: $0 {name} {env} {cluster_name} {region}"
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 {name} {env}"
     exit 1
 fi
 
 # Assign input variables
 name=$1
 env=$2
-cluster_name=$3
-region=$4
+cluster_name="your-cluster-name"
 
-# Get the corresponding task definition with the latest revision
-task_definition=$(aws ecs list-task-definitions --family-prefix $name --sort DESC --region $region | jq -r '.taskDefinitionArns[0]')
+# Get the latest task definition ARN by name
+task_definition=$(aws ecs list-task-definitions --family-prefix $name --sort DESC | jq -r '.taskDefinitionArns[0]')
 
-# Run the task in the specified environment
-result=$(aws ecs run-task --task-definition $task_definition --launch-type FARGATE --cluster $cluster_name --region $region)
+# Run the task in the specified environment and cluster
+result=$(aws ecs run-task --task-definition $task_definition --launch-type FARGATE --cluster $cluster_name)
 
 # Extract the task ID
 task_id=$(echo $result | jq -r '.tasks[0].taskArn' | cut -d '/' -f 2)
 
 # Get the task status
-task_status=$(aws ecs describe-tasks --tasks $task_id --cluster $cluster_name --region $region | jq -r '.tasks[0].lastStatus')
+task_status=$(aws ecs describe-tasks --tasks $task_id --cluster $cluster_name | jq -r '.tasks[0].lastStatus')
 
 # Form the link to the log group
-log_group_link="https://console.aws.amazon.com/cloudwatch/home?region=$region#logsV2:log-groups/log-group/"
+log_group_link="https://console.aws.amazon.com/cloudwatch/home#logsV2:log-groups/log-group/"
 
 # Display the task status and log group link
 if [ "$task_status" == "RUNNING" ]; then

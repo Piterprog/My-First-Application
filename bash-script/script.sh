40eec633a8533284279eef58af8d4623558b7711
Piter <152597719+Piterprog@users.noreply.github.com>
2024-05-13 11:28:27 +0300

    Update script.sh

--- bash-script/script.sh
+++ bash-script/script.sh
@@ -14,22 +14,31 @@ security_group_id=$4
 service_name=$5
 
 # Создаем новый сервис ECS
-aws ecs create-service --cluster $cluster --service-name $service_name --task-definition $task_definition
+echo "Создание сервиса в Amazon ECS..."
+aws ecs create-service --cluster "$cluster" --service-name "$service_name" --task-definition "$task_definition"
 
 # Обновляем настройки сервиса ECS с указанными VPC и Security Group
-aws ecs update-service --cluster $cluster --service $service_name --network-configuration "awsvpcConfiguration={subnets=[$subnet_id],securityGroups=[$security_group_id]}"
+echo "Обновление настроек сервиса ECS..."
+aws ecs update-service --cluster "$cluster" --service "$service_name" --network-configuration "awsvpcConfiguration={subnets=[$subnet_id],securityGroups=[$security_group_id]}"
 
-# Получаем последнюю ARN определения задачи по имени
-task_definition=$(aws ecs list-task-definitions --family-prefix $task_definition --sort DESC | jq -r '.taskDefinitionArns[0]')
+# Запускаем сервис
+echo "Запуск сервиса..."
+aws ecs update-service --cluster "$cluster" --service "$service_name" --desired-count 1
 
-# Запускаем задачу в указанной среде
-result=$(aws ecs run-task --task-definition $task_definition --launch-type FARGATE --cluster $cluster)
+# Получаем последнюю версию задачи по имени
+echo "Получение последней версии задачи по имени..."
+task_definition=$(aws ecs list-task-definitions --family-prefix "$task_definition" --sort DESC | jq -r '.taskDefinitionArns[0]')
+
+# Запускаем задачу в указанном окружении
+echo "Запуск задачи в Amazon ECS..."
+result=$(aws ecs run-task --task-definition "$task_definition" --launch-type FARGATE --cluster "$cluster")
 
 # Извлекаем идентификатор задачи
-task_id=$(echo $result | jq -r '.tasks[0].taskArn' | cut -d '/' -f 2)
+task_id=$(echo "$result" | jq -r '.tasks[0].taskArn' | cut -d '/' -f 2)
 
 # Получаем статус задачи
-task_status=$(aws ecs describe-tasks --tasks $task_id --cluster $cluster | jq -r '.tasks[0].lastStatus')
+echo "Получение статуса задачи..."
+task_status=$(aws ecs describe-tasks --tasks "$task_id" --cluster "$cluster" | jq -r '.tasks[0].lastStatus')
 
 # Формируем ссылку на группу журналов
 log_group_link="https://console.aws.amazon.com/cloudwatch/home#logsV2:log-groups/log-group/"

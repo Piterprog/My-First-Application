82c9c4cc5386bec2e183fb51afca47bc5dbaf5ea
Piter <152597719+Piterprog@users.noreply.github.com>
2024-05-13 12:46:23 +0300

    Update script.sh

--- bash-script/script.sh
+++ bash-script/script.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 
-# Проверяем, правильное ли количество аргументов передано
-if [ "$#" -ne 5 ]; then
-    echo "Использование: $0 {cluster} {task-definition} {subnet-id} {security-group-id} {service-name}"
+# Check if the correct number of arguments is provided
+if [ "$#" -ne 6 ]; then
+    echo "Usage: $0 {cluster} {task-definition} {subnet-id} {security-group-id} {service-name} {vpc-id}"
     exit 1
 fi
 
-# Присваиваем входные данные переменным
+# Assign input variables
 cluster=$1
 task_definition=$2
 subnet_id=$3
 security_group_id=$4
 service_name=$5
+vpc_id=$6
 
-# Создаем новый сервис ECS
-echo "Создание сервиса в Amazon ECS..."
-aws ecs create-service --cluster "$cluster" --service-name "$service_name" --task-definition "$task_definition" --desired-count 1 --network-configuration "awsvpcConfiguration={subnets=[$subnet_id],securityGroups=[$security_group_id]}"
+# Create a new ECS service
+echo "Creating service in Amazon ECS..."
+aws ecs create-service --cluster "$cluster" --service-name "$service_name" --task-definition "$task_definition" --desired-count 1 --network-configuration "awsvpcConfiguration={subnets=[$subnet_id],securityGroups=[$security_group_id],assignPublicIp=ENABLED}" --vpc $vpc_id
 
 # Получаем последнюю версию задачи по имени
 task_definition=$(aws ecs list-task-definitions --family-prefix "$task_definition" --sort DESC | jq -r '.taskDefinitionArns[0]')
 
 # Запускаем задачу в указанной среде
 echo "Запуск задачи в Amazon ECS..."
-result=$(aws ecs run-task --task-definition "$task_definition" --launch-type FARGATE --cluster "$cluster" --network-configuration "awsvpcConfiguration={subnets=[$subnet_id],securityGroups=[$security_group_id]}")
+result=$(aws ecs run-task --task-definition "$task_definition" --launch-type FARGATE --cluster "$cluster" --network-configuration "awsvpcConfiguration={subnets=[$subnet_id],securityGroups=[$security_group_id],assignPublicIp=ENABLED}" --vpc $vpc_id)
 
 # Извлекаем идентификатор задачи
 task_id=$(echo "$result" | jq -r '.tasks[0].taskArn' | cut -d '/' -f 2)
@@ -44,7 +45,6 @@ elif [ "$task_status" == "STOPPED" ]; then
 else
     echo "Не удалось запустить задачу."
 fi
-
 
 
 

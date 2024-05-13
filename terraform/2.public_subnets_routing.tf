62d7dd8ca01463bb8037a82fc9dcd6344b589090
Piter Kononihin <Piterbog@gmail.com>
2024-05-13 16:54:03 +0300

    create vpc

--- terraform/2.public_subnets_routing.tf
+++ terraform/2.public_subnets_routing.tf
@@ -23,7 +23,6 @@ resource "aws_route_table" "public_subnets" {
     }
 }
 
-
 resource "aws_route_table_association" "public_routes" {
   count          = length(aws_subnet.public_subnets[*].id)
   route_table_id = aws_route_table.public_subnets[count.index].id

e381ef0d030e27f3447b5173f46327651497c262
Piter Kononihin <Piterbog@gmail.com>
2024-05-13 17:12:23 +0300

    dsds

--- terraform/2.public_subnets_routing.tf
+++ terraform/2.public_subnets_routing.tf
@@ -25,7 +25,7 @@ resource "aws_route_table" "public_subnets" {
 
 resource "aws_route_table_association" "public_routes" {
   count          = length(aws_subnet.public_subnets[*].id)
-  route_table_id = aws_route_table.public_subnets[count.index +1].id
-   subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
+  route_table_id = aws_route_table.public_subnets[count.index].id
+  subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
 }
 

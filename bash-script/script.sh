ace1cde7db43868f61d4e6843521210424358090
Piter <152597719+Piterprog@users.noreply.github.com>
2024-05-13 11:48:26 +0300

    Update script.sh

--- bash-script/script.sh
+++ bash-script/script.sh
@@ -1,55 +1,50 @@
 #!/bin/bash
 
-# Проверяем, правильное ли количество аргументов передано
+# Check if the correct number of arguments is provided
 if [ "$#" -ne 5 ]; then
-    echo "Использование: $0 {cluster} {task-definition} {subnet-id} {security-group-id} {service-name}"
+    echo "Usage: $0 {cluster} {task-definition} {subnet-id} {security-group-id} {service-name}"
     exit 1
 fi
 
-# Присваиваем входные данные переменным
+# Assign input variables
 cluster=$1
 task_definition=$2
 subnet_id=$3
 security_group_id=$4
 service_name=$5
 
-# Создаем новый сервис ECS
-echo "Создание сервиса в Amazon ECS..."
-aws ecs create-service --cluster "$cluster" --service-name "$service_name" --task-definition "$task_definition" --desired-count 1
+# Create a new ECS service
+echo "Creating service in Amazon ECS..."
+aws ecs create-service --cluster "$cluster" --service-name "$service_name" --task-definition "$task_definition" --desired-count 1 --network-configuration "awsvpcConfiguration={subnets=[$subnet_id],securityGroups=[$security_group_id]}"
 
-# Обновляем настройки сервиса ECS с указанными VPC и Security Group
-echo "Обновление настроек сервиса ECS..."
-aws ecs update-service --cluster "$cluster" --service "$service_name" --network-configuration "awsvpcConfiguration={subnets=[$subnet_id],securityGroups=[$security_group_id]}"
-
-# Запускаем сервис
-echo "Запуск сервиса..."
+# Start the service
+echo "Starting the service..."
 aws ecs update-service --cluster "$cluster" --service "$service_name" --desired-count 1
 
-# Получаем последнюю версию задачи по имени
-echo "Получение последней версии задачи по имени..."
+# Get the latest task definition ARN by name
 task_definition=$(aws ecs list-task-definitions --family-prefix "$task_definition" --sort DESC | jq -r '.taskDefinitionArns[0]')
 
-# Запускаем задачу в указанном окружении
-echo "Запуск задачи в Amazon ECS..."
+# Run the task in the specified environment
+echo "Running task in Amazon ECS..."
 result=$(aws ecs run-task --task-definition "$task_definition" --launch-type FARGATE --cluster "$cluster" --network-configuration "awsvpcConfiguration={subnets=[$subnet_id],securityGroups=[$security_group_id]}")
 
-# Извлекаем идентификатор задачи
+# Extract the task ID
 task_id=$(echo "$result" | jq -r '.tasks[0].taskArn' | cut -d '/' -f 2)
 
-# Получаем статус задачи
-echo "Получение статуса задачи..."
+# Get the task status
+echo "Getting task status..."
 task_status=$(aws ecs describe-tasks --tasks "$task_id" --cluster "$cluster" | jq -r '.tasks[0].lastStatus')
 
-# Формируем ссылку на группу журналов
+# Form the link to the log group
 log_group_link="https://console.aws.amazon.com/cloudwatch/home#logsV2:log-groups/log-group/"
 
-# Выводим статус задачи и ссылку на группу журналов
+# Display the task status and log group link
 if [ "$task_status" == "RUNNING" ]; then
-    echo "Задача в данный момент выполняется."
-    echo "Ссылка на группу журналов: $log_group_link"
+    echo "The task is currently running."
+    echo "Log Group Link: $log_group_link"
 elif [ "$task_status" == "STOPPED" ]; then
-    echo "Задача остановлена."
-    echo "Ссылка на группу журналов: $log_group_link"
+    echo "The task has stopped."
+    echo "Log Group Link: $log_group_link"
 else
-    echo "Не удалось запустить задачу."
+    echo "Failed to start the task."
 fi

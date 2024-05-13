456029ed5f8e4110c5cec2c76580862553cf0bce
Piter Kononihin <Piterbog@gmail.com>
2024-05-13 17:09:42 +0300

    cretae

--- terraform/2.public_subnets_routing.tf
+++ terraform/2.public_subnets_routing.tf
@@ -26,5 +26,5 @@ resource "aws_route_table" "public_subnets" {
 resource "aws_route_table_association" "public_routes" {
   count          = length(aws_subnet.public_subnets[*].id)
   route_table_id = aws_route_table.public_subnets[count.index].id
-  subnet_id      = aws_subnet.public_subnets[count.index].id
+   subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
 }

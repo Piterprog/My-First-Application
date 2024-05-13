118279384d33dd075d66ca5ce1f8a6afe328c442
Piter Kononihin <Piterbog@gmail.com>
2024-05-13 16:21:26 +0300

    changes all commits

--- bash-script/script.sh
+++ bash-script/script.sh
@@ -18,32 +18,32 @@ vpc_id=$6
 echo "Creating service in Amazon ECS..."
 aws ecs create-service --cluster "$cluster" --service-name "$service_name" --task-definition "$task_definition" --desired-count 1 --network-configuration "awsvpcConfiguration={subnets=[$subnet_id],securityGroups=[$security_group_id],assignPublicIp=ENABLED}" --vpc $vpc_id
 
-# Получаем последнюю версию задачи по имени
+# Get the latest task definition ARN by name
 task_definition=$(aws ecs list-task-definitions --family-prefix "$task_definition" --sort DESC | jq -r '.taskDefinitionArns[0]')
 
-# Запускаем задачу в указанной среде
-echo "Запуск задачи в Amazon ECS..."
+# Run the task in the specified environment
+echo "Running task in Amazon ECS..."
 result=$(aws ecs run-task --task-definition "$task_definition" --launch-type FARGATE --cluster "$cluster" --network-configuration "awsvpcConfiguration={subnets=[$subnet_id],securityGroups=[$security_group_id],assignPublicIp=ENABLED}" --vpc $vpc_id)
 
-# Извлекаем идентификатор задачи
+# Extract the task ID
 task_id=$(echo "$result" | jq -r '.tasks[0].taskArn' | cut -d '/' -f 2)
 
-# Получаем статус задачи
-echo "Получение статуса задачи..."
+# Get the task status
+echo "Getting task status..."
 task_status=$(aws ecs describe-tasks --tasks "$task_id" --cluster "$cluster" | jq -r '.tasks[0].lastStatus')
 
-# Формируем ссылку на группу журналов
+# Form the link to the log group
 log_group_link="https://console.aws.amazon.com/cloudwatch/home#logsV2:log-groups/log-group/"
 
-# Отображаем статус задачи и ссылку на группу журналов
+# Display the task status and log group link
 if [ "$task_status" == "RUNNING" ]; then
-    echo "Задача в данный момент выполняется."
-    echo "Ссылка на группу журналов: $log_group_link"
+    echo "The task is currently running."
+    echo "Log Group Link: $log_group_link"
 elif [ "$task_status" == "STOPPED" ]; then
-    echo "Задача остановлена."
-    echo "Ссылка на группу журналов: $log_group_link"
+    echo "The task has stopped."
+    echo "Log Group Link: $log_group_link"
 else
-    echo "Не удалось запустить задачу."
+    echo "Failed to start the task."
 fi
 
 

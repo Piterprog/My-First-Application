0395f97b1b200bad2982b8e715414d32ce9717a1
Piter <152597719+Piterprog@users.noreply.github.com>
2024-05-13 09:13:56 +0300

    Update script.sh

--- bash-script/script.sh
+++ bash-script/script.sh
@@ -9,19 +9,18 @@ fi
 # Assign input variables
 name=$1
 env=$2
-cluster_name="your-cluster-name"
 
 # Get the latest task definition ARN by name
 task_definition=$(aws ecs list-task-definitions --family-prefix $name --sort DESC | jq -r '.taskDefinitionArns[0]')
 
-# Run the task in the specified environment and cluster
-result=$(aws ecs run-task --task-definition $task_definition --launch-type FARGATE --cluster $cluster_name)
+# Run the task in the specified environment
+result=$(aws ecs run-task --task-definition $task_definition --launch-type FARGATE --cluster $env)
 
 # Extract the task ID
 task_id=$(echo $result | jq -r '.tasks[0].taskArn' | cut -d '/' -f 2)
 
 # Get the task status
-task_status=$(aws ecs describe-tasks --tasks $task_id --cluster $cluster_name | jq -r '.tasks[0].lastStatus')
+task_status=$(aws ecs describe-tasks --tasks $task_id --cluster $env | jq -r '.tasks[0].lastStatus')
 
 # Form the link to the log group
 log_group_link="https://console.aws.amazon.com/cloudwatch/home#logsV2:log-groups/log-group/"

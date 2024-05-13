e73ee1a99c705c558421d239d90d65f8141c67cd
Piter Kononihin <Piterbog@gmail.com>
2024-05-13 17:16:24 +0300

    dasdasd

--- terraform/2.public_subnets_routing.tf
+++ terraform/2.public_subnets_routing.tf
@@ -29,8 +29,4 @@ resource "aws_route_table_association" "public_routes" {
   subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
 }
 
-resource "aws_route_table_association" "public_routes" {
-  count          = length(aws_subnet.public_subnets[*].id)
-  route_table_id = aws_route_table.public_subnets.id
-  subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
-}
+

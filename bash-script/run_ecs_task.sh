722bbd784a839a92afa3dd8318039e8292731487
piterKononihin <piter@emporus.com>
2024-05-15 22:40:45 +0300

    last changes

---
+++ bash-script/run_ecs_task.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Command line parameters
+SERVICE_NAME=$1  # The name of the task or task family
+ENVIRONMENT=$2  # The environment (e.g., production, staging)
+CLUSTER=$3  # The name of the ECS cluster
+SECURITY_GROUP=$4  # The security group ID
+SUBNET=$5  # The subnet ID
+VPC_ID=$6  # The VPC ID
+
+# Constant values
+REGION="us-east-1"  # Specified AWS region
+LOG_PREFIX="/ecs"
+
+# Define the log group name
+LOG_GROUP="$LOG_PREFIX/$SERVICE_NAME/$ENVIRONMENT"
+
+# Check if the log group exists
+LOG_GROUP_EXISTS=$(aws logs describe-log-groups --log-group-name-prefix $LOG_GROUP --region $REGION --query "logGroups[?logGroupName=='$LOG_GROUP'].logGroupName" --output text)
+
+# Create the log group if it does not exist
+if [ -z "$LOG_GROUP_EXISTS" ]; then
+  aws logs create-log-group --log-group-name $LOG_GROUP --region $REGION
+  echo "Log group created: $LOG_GROUP"
+else
+  echo "Log group already exists: $LOG_GROUP"
+fi
+
+# Fetching the latest revision of the task definition
+TASK_DEFINITION=$(aws ecs list-task-definitions --family-prefix $SERVICE_NAME --sort DESC --query 'taskDefinitionArns[0]' --output text --region $REGION)
+
+# Running the ECS task
+RUN_TASK_OUTPUT=$(aws ecs run-task --cluster $CLUSTER --launch-type FARGATE --task-definition $TASK_DEFINITION --network-configuration "awsvpcConfiguration={subnets=[$SUBNET],securityGroups=[$SECURITY_GROUP],assignPublicIp=ENABLED}" --region $REGION)
+
+# Extracting the Task ARN from the output
+TASK_ARN=$(echo $RUN_TASK_OUTPUT | jq -r '.tasks[0].taskArn')
+
+# Constructing the log group link
+LOG_GROUP_LINK="https://console.aws.amazon.com/cloudwatch/home?region=$REGION#logsV2:log-groups/log-group/$LOG_GROUP"
+
+# Outputting the Task ARN and log group link
+echo "Task ARN: $TASK_ARN"
+echo "Log Group Link: $LOG_GROUP_LINK"
+

9c03cd5c4440f4b1b7fb56c8eb506de224a292b4
Piter <152597719+Piterprog@users.noreply.github.com>
2024-05-16 07:19:49 +0300

    Update run_ecs_task.sh

--- bash-script/run_ecs_task.sh
+++ bash-script/run_ecs_task.sh
@@ -4,13 +4,29 @@
 SERVICE_NAME=$1  # The name of the task or task family
 ENVIRONMENT=$2  # The environment (e.g., production, staging)
 CLUSTER=$3  # The name of the ECS cluster
-SECURITY_GROUP=$4  # The security group ID
-SUBNET=$5  # The subnet ID
-VPC_ID=$6  # The VPC ID
+SECURITY_GROUP=${4:-default}  # The security group ID (default based on environment)
+SUBNET=${5:-default}  # The subnet ID (default based on environment)
+VPC_ID=${6:-default}  # The VPC ID (default based on environment)
 
 # Constant values
 REGION="us-east-1"  # Specified AWS region
 LOG_PREFIX="/ecs"
+
+# Define default values based on environment
+if [ "$ENVIRONMENT" == "production" ]; then
+  DEFAULT_SECURITY_GROUP="production-ecs-svc-sg"
+  DEFAULT_SUBNET="subnet-prod-default"  # Specify the default subnet value for production
+  DEFAULT_VPC_ID="vpc-prod-default"  # Specify the default VPC value for production
+else
+  DEFAULT_SECURITY_GROUP=""
+  DEFAULT_SUBNET="subnet-staging-default"  # Specify the default subnet value for staging
+  DEFAULT_VPC_ID="vpc-staging-default"  # Specify the default VPC value for staging
+fi
+
+# Override default values if not provided
+SECURITY_GROUP=${SECURITY_GROUP:-$DEFAULT_SECURITY_GROUP}
+SUBNET=${SUBNET:-$DEFAULT_SUBNET}
+VPC_ID=${VPC_ID:-$DEFAULT_VPC_ID}
 
 # Define the log group name
 LOG_GROUP="$LOG_PREFIX/$SERVICE_NAME/$ENVIRONMENT"
@@ -30,7 +46,7 @@ fi
 TASK_DEFINITION=$(aws ecs list-task-definitions --family-prefix $SERVICE_NAME --sort DESC --query 'taskDefinitionArns[0]' --output text --region $REGION)
 
 # Running the ECS task
-RUN_TASK_OUTPUT=$(aws ecs run-task --cluster $CLUSTER --launch-type FARGATE --task-definition $TASK_DEFINITION --network-configuration "awsvpcConfiguration={subnets=[$SUBNET],securityGroups=[$SECURITY_GROUP],assignPublicIp=ENABLED}" --region $REGION)
+RUN_TASK_OUTPUT=$(aws ecs run-task --cluster $CLUSTER --launch-type FARGATE --task-definition $TASK_DEFINITION --network-configuration "awsvpcConfiguration={subnets=[$SUBNET],securityGroups=[$SECURITY_GROUP],assignPublicIp=DISABLED}" --region $REGION)
 
 # Extracting the Task ARN from the output
 TASK_ARN=$(echo $RUN_TASK_OUTPUT | jq -r '.tasks[0].taskArn')

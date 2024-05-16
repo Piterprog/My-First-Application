a4477239840e8848eeac74c38e5dea8e30701369
Piter <152597719+Piterprog@users.noreply.github.com>
2024-05-16 11:23:10 +0300

    Update run_ecs_task.sh

--- bash-script/run_ecs_task.sh
+++ bash-script/run_ecs_task.sh
@@ -43,6 +43,7 @@ echo "Subnet: $SUBNET"
 echo "VPC ID: $VPC_ID"
 
 # Verify that the subnet exists
+echo "Checking if subnet exists..."
 SUBNET_EXISTS=$(aws ec2 describe-subnets --subnet-ids $SUBNET --region $REGION --query 'Subnets[0].SubnetId' --output text)
 if [ -z "$SUBNET_EXISTS" ]; then
   echo "Error: Subnet ID '$SUBNET' does not exist."
@@ -51,6 +52,7 @@ fi
 
 # Verify that the security group exists
 if [ -n "$SECURITY_GROUP" ]; then
+  echo "Checking if security group exists..."
   SECURITY_GROUP_EXISTS=$(aws ec2 describe-security-groups --group-ids $SECURITY_GROUP --region $REGION --query 'SecurityGroups[0].GroupId' --output text)
   if [ -z "$SECURITY_GROUP_EXISTS" ]; then
     echo "Error: Security Group ID '$SECURITY_GROUP' does not exist."
@@ -62,17 +64,19 @@ fi
 LOG_GROUP="$LOG_PREFIX/$SERVICE_NAME/$ENVIRONMENT"
 
 # Check if the log group exists
+echo "Checking if log group exists..."
 LOG_GROUP_EXISTS=$(aws logs describe-log-groups --log-group-name-prefix $LOG_GROUP --region $REGION --query "logGroups[?logGroupName=='$LOG_GROUP'].logGroupName" --output text)
 
 # Create the log group if it does not exist
 if [ -z "$LOG_GROUP_EXISTS" ]; then
+  echo "Creating log group: $LOG_GROUP"
   aws logs create-log-group --log-group-name $LOG_GROUP --region $REGION
-  echo "Log group created: $LOG_GROUP"
 else
   echo "Log group already exists: $LOG_GROUP"
 fi
 
 # Fetching the latest revision of the task definition
+echo "Fetching the latest task definition..."
 TASK_DEFINITION=$(aws ecs list-task-definitions --family-prefix $SERVICE_NAME --sort DESC --query 'taskDefinitionArns[0]' --output text --region $REGION)
 if [ -z "$TASK_DEFINITION" ]; then
   echo "Error: Could not retrieve the latest task definition for service: $SERVICE_NAME"
@@ -82,9 +86,11 @@ fi
 echo "Using task definition: $TASK_DEFINITION"
 
 # Running the ECS task
+echo "Running the ECS task..."
 RUN_TASK_OUTPUT=$(aws ecs run-task --cluster $CLUSTER --launch-type FARGATE --task-definition $TASK_DEFINITION --network-configuration "awsvpcConfiguration={subnets=[$SUBNET],securityGroups=[$SECURITY_GROUP],assignPublicIp=DISABLED}" --region $REGION)
 if [ $? -ne 0 ]; then
   echo "Error: Failed to run task"
+  echo "RUN_TASK_OUTPUT: $RUN_TASK_OUTPUT"
   exit 1
 fi
 
@@ -92,6 +98,7 @@ fi
 TASK_ARN=$(echo $RUN_TASK_OUTPUT | jq -r '.tasks[0].taskArn')
 if [ -z "$TASK_ARN" ]; then
   echo "Error: Task ARN not found in the output"
+  echo "RUN_TASK_OUTPUT: $RUN_TASK_OUTPUT"
   exit 1
 fi
 
@@ -101,6 +108,4 @@ LOG_GROUP_LINK="https://console.aws.amazon.com/cloudwatch/home?region=$REGION#lo
 # Outputting the Task ARN and log group link
 echo "Task ARN: $TASK_ARN"
 echo "Log Group Link: $LOG_GROUP_LINK"
-
-
 

6d01e346091cdf1acbb7c896747dbbe8619e803e
piterKononihin <piter@emporus.com>
2024-05-22 17:57:09 +0300

    dsdd

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -10,22 +10,6 @@ output "vpc_cidr_id" {
   value = module.vpc_staging.vpc_cidr_id
 }
 
-output "vpc_id" {
-  value = module.vpc_staging.vpc_id
-}
-
-output "vpc_cidr" {
-  value = module.vpc_staging.vpc_cidr_id
-}
-
-output "public_subnet_ids" {
-  value = module.vpc_staging.public_subnets
-}
-
-output "private_subnet_ids" {
-  value = module.vpc_staging.private_subnets
-}
-
 output "security_group_id" {
   value = module.vpc_staging.security_group_id
 }

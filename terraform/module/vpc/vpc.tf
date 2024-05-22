98c35044ca24aa79d802b6a1815e7c73f743a8ef
Piter <152597719+Piterprog@users.noreply.github.com>
2024-05-22 11:38:07 +0300

    Update vpc.tf

--- terraform/module/vpc/vpc.tf
+++ terraform/module/vpc/vpc.tf
@@ -1,8 +1,10 @@
 
-resource "aws_vpc" "module" {
-  cidr_block = "10.0.0.0/16"
-
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
   tags = {
-    Name  = "module"
+    Name = "${var.env}-vpc"
   }
 }
+

53644429f5144e6e3ccb36f8df8af151d5809e6c
Piter <152597719+Piterprog@users.noreply.github.com>
2024-05-22 12:33:42 +0300

    Update variables.tf

--- terraform/module/vpc/variables.tf
+++ terraform/module/vpc/variables.tf
@@ -7,3 +7,23 @@ variable "vpc_cidr" {
   default = "10.0.0.0/16"
 }
 
+variable "public_subnet_cidrs" {
+  default = [
+    "10.0.10.0/24",
+    "10.0.20.0/24"
+  ]
+}
+
+variable "private_subnet_cidrs" {
+  default = [
+    "10.0.11.0/24",
+    "10.0.21.0/24"
+  ]
+}
+
+variable "database_subnet_cidrs" {
+  default = [
+    "10.0.12.0/24",
+    "10.0.22.0/24"
+  ]
+}

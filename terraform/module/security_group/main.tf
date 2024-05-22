e894b80160758635abb014b6eb3024d32ddf0233
Piter <152597719+Piterprog@users.noreply.github.com>
2024-05-22 14:24:38 +0300

    Update main.tf

--- terraform/module/security_group/main.tf
+++ terraform/module/security_group/main.tf
@@ -28,54 +28,6 @@ resource "aws_security_group" "security_group_musad" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    description = "Application-container"
-    from_port   = 3000
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "Database-port"
-    from_port   = 3306
-    to_port     = 3306
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]
-  }
-
-  ingress {
-    description = "node-exporter"
-    from_port   = 9100
-    to_port     = 9100
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "cadvisor-exporter"
-    from_port   = 9200
-    to_port     = 9200
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "prometheus"
-    from_port   = 9090
-    to_port     = 9090
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "grafana"
-    from_port   = 3000
-    to_port     = 3000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   egress {
     from_port       = 0
     to_port         = 0

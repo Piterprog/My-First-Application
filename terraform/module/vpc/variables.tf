41897c7e34de0cca303735135fe6796989a1d6b1
Piter <152597719+Piterprog@users.noreply.github.com>
2024-05-22 13:45:45 +0300

    Update variables.tf

--- terraform/module/vpc/variables.tf
+++ terraform/module/vpc/variables.tf
@@ -1,29 +1,19 @@
-
 variable "env" {
   default = "staging"
 }
 
 variable "vpc_cidr" {
-  default = "10.0.0.0/16"
+  type = string
 }
 
 variable "public_subnet_cidrs" {
-  default = [
-    "10.0.10.0/24",
-    "10.0.20.0/24"
-  ]
+  type = list(string)
 }
 
 variable "private_subnet_cidrs" {
-  default = [
-    "10.0.11.0/24",
-    "10.0.21.0/24"
-  ]
+  type = list(string)
 }
 
 variable "database_subnet_cidrs" {
-  default = [
-    "10.0.12.0/24",
-    "10.0.22.0/24"
-  ]
+  type = list(string)
 }

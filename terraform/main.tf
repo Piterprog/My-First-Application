d2ccc5ae96a480964eb5cba1b6900f7482235a06
Piter <152597719+Piterprog@users.noreply.github.com>
2024-05-22 15:52:23 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,6 @@
 module "vpc_staging" {
   source                   = "./module/vpc"
+  env                      = "staging"
   vpc_cidr                 = "10.100.0.0/16"
   public_subnet_cidrs      = ["10.100.1.0/24", "10.100.2.0/24"]
   private_subnet_cidrs     = ["10.100.10.0/24", "10.100.22.0/24"]

6e32ead79a4750cb839e62b792e5c5f3ac5f0f0d
piterKononihin <piter@emporus.com>
2024-05-22 17:47:14 +0300

    dsds

--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -1,18 +1,17 @@
-
-output "vpc_id" {
-  value = aws_vpc.main.id
-}
-
-output "vpc_cidr" {
-  value = aws_vpc.main.cidr_block
-}
-
-output "public_subnet_ids" {
+output "public_subnets" {
   value = aws_subnet.public_subnets[*].id
 }
 
-output "private_subnet_ids" {
+output "private_subnets" {
   value = aws_subnet.private_subnets[*].id
+}
+
+output "vpc_cidr_id" {
+  value = aws_vpc.main.cidr_block
+}
+
+output "vpc_id" {
+  value = aws_vpc.main.id
 }
 
 output "security_group_id" {
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -15,7 +15,7 @@ output "vpc_id" {
 }
 
 output "vpc_cidr" {
-  value = module.vpc_staging.vpc_cidr
+  value = module.vpc_staging.vpc_cidr_id
 }
 
 output "public_subnet_ids" {
@@ -24,10 +24,6 @@ output "public_subnet_ids" {
 
 output "private_subnet_ids" {
   value = module.vpc_staging.private_subnets
-}
-
-output "database_subnet_ids" {
-  value = module.vpc_staging.database_subnets
 }
 
 output "security_group_id" {

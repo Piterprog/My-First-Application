efaf1945a6f06b26d1cc268903f870b12ac5d382
Piter <152597719+Piterprog@users.noreply.github.com>
2024-05-27 07:21:01 +0300

    Update main.tf

--- terraform/modules/sg_group/main.tf
+++ terraform/modules/sg_group/main.tf
@@ -1,1 +1,23 @@
+resource "aws_security_group" "this" {
+  name        = var.sg_name
+  description = var.sg_desctiption
+  vpc_id      = var.vpc 
 
+  ingress {
+    from_port   = var.ingress_from_port
+    to_port     = var.ingress_to_port
+    protocol    = var.ingress_protocol
+    cidr_blocks = var.ingress_cidr_block
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = var.tag
+  }
+}

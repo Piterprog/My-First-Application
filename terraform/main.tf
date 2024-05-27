e28f99c3af8ee16d44f8729b81f9d78b458c9bab
Piter <152597719+Piterprog@users.noreply.github.com>
2024-05-27 10:22:58 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,10 +33,22 @@ module "my_security_group" {
       to_port     = 443
       protocol    = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+      from_port   = 3306
+      to_port     = 3306
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
     }
   ]
-
   tags = {
     Name = "my-security-group"
   }
 }
+

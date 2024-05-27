ea64a52c16f53c32739d9e144f8beec2baca6de5
Piter <152597719+Piterprog@users.noreply.github.com>
2024-05-27 07:31:33 +0300

    Update veriables.tf

--- terraform/modules/sg_group/veriables.tf
+++ terraform/modules/sg_group/veriables.tf
@@ -1,1 +1,25 @@
-
+variable "vpc_id" {
+  type = string
+}
+variable "ingress_from_port" {
+  type = number
+}
+variable "ingress_to_port" {
+  type = number
+}
+variable "ingress_protocol" {
+ type = string
+}
+variable "ingress_cidr_blocks" {
+  type = list(string)
+}
+variable "tags" {
+  type = map(string)
+  default = {}
+}
+variable "sg_name" {
+  type = string
+}
+variable "sg_description" {
+  type = string
+}

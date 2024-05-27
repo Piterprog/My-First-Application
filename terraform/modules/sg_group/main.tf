77e6e3e162ad9523428e305cfd4302af848e3fc8
Piter <152597719+Piterprog@users.noreply.github.com>
2024-05-27 10:21:45 +0300

    Update main.tf

--- terraform/modules/sg_group/main.tf
+++ terraform/modules/sg_group/main.tf
@@ -1,25 +1,24 @@
 resource "aws_security_group" "this" {
   name        = var.sg_name
-  description = var.sg_desctiption
-  vpc_id      = var.vpc 
+  description = var.sg_description
+  vpc_id      = var.vpc_id
 
- dynamic "ingress" {
-  for_each = var.ingress_rules
-  content {
-    from_port   = ingress_value.from_port
-    to_port     = ingress_value.to_port
-    protocol    = ingress_value.protocol
-    cidr_blocks = ingress_value.cidr_blocks  
+  dynamic "ingress" {
+    for_each = var.ingress_rules
+    content {
+      from_port   = ingress.value.from_port
+      to_port     = ingress.value.to_port
+      protocol    = ingress.value.protocol
+      cidr_blocks = ingress.value.cidr_blocks
+    }
   }
-}
+
   egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-  tags = {
-    Name = var.tag
-  }
+  tags = var.tags
 }

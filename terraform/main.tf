2e988ec7f3addeefa2a5efaeb93457a676a3f790
Piter <152597719+Piterprog@users.noreply.github.com>
2024-05-27 10:13:21 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,6 +8,35 @@ module "vpc_staging" {
   env                 = "staging"
   vpc_cidr            = "10.100.0.0/16"
   public_subnet_cidrs = ["10.100.1.0/24", "10.100.2.0/24"]
-  private_subnet_cidrs= ["10.100.10.0/24", "10.100.22.0/24"]
+  private_subnet_cidrs = ["10.100.10.0/24", "10.100.22.0/24"]
 }
 
+data "aws_vpc" "staging" {
+  id = module.vpc_staging.vpc_id
+}
+
+module "my_security_group" {
+  source = "./modules/sg_group"
+
+  sg_name        = "musad"
+  sg_description = "Security group for my application"
+  vpc_id         = data.aws_vpc.staging.id
+  ingress_rules = [
+    {
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+      from_port   = 443
+      to_port     = 443
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  ]
+
+  tags = {
+    Name = "my-security-group"
+  }
+}

187ef3d23006bff0274ed59d4c23df776685d00e
Piter <152597719+Piterprog@users.noreply.github.com>
2024-05-29 08:12:12 +0300

    Update veriables.tf

--- terraform/modules/sg_group/veriables.tf
+++ terraform/modules/sg_group/veriables.tf
@@ -1,20 +1,16 @@
 variable "sg_name" {
-  description = "The name of the security group"
-  type        = string
+  type = string
 }
 
 variable "sg_description" {
-  description = "The description of the security group"
-  type        = string
+  type = string
 }
 
 variable "vpc_id" {
-  description = "The VPC ID where the security group will be created"
-  type        = string
+  type = string
 }
 
 variable "ingress_rules" {
-  description = "List of ingress rules for the security group"
   type = list(object({
     from_port   = number
     to_port     = number
@@ -22,10 +18,3 @@ variable "ingress_rules" {
     cidr_blocks = list(string)
   }))
 }
-
-variable "tags" {
-  description = "A map of tags to assign to the resource"
-  type        = map(string)
-  default     = {}
-}
-

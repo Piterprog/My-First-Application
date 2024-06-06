3d9b403564eace15f83330000926a9022bb1e032
Piter <152597719+Piterprog@users.noreply.github.com>
2024-06-06 10:26:27 +0300

    Update run_ecs_task.sh

--- bash-script/run_ecs_task.sh
+++ bash-script/run_ecs_task.sh
@@ -40,17 +40,15 @@ echo "VPC with ID $VPC_ID exists."
 
 # Check if primary subnet exists
 if ! aws ec2 describe-subnets --subnet-ids $PRIMARY_SUBNET --region $REGION > /dev/null 2>&1; then
-  echo "Error: Primary subnet with ID $PRIMARY_SUBNET not found."
-  exit 1
+  echo "Primary subnet with ID $PRIMARY_SUBNET not found. Trying secondary subnet..."
+  PRIMARY_SUBNET=$SECONDARY_SUBNET
 fi
-echo "Primary subnet with ID $PRIMARY_SUBNET exists."
+echo "Using primary subnet: $PRIMARY_SUBNET"
 
 # Check if secondary subnet exists
 if ! aws ec2 describe-subnets --subnet-ids $SECONDARY_SUBNET --region $REGION > /dev/null 2>&1; then
-  echo "Error: Secondary subnet with ID $SECONDARY_SUBNET not found."
-  exit 1
+  echo "Secondary subnet with ID $SECONDARY_SUBNET not found. Using primary subnet..."
 fi
-echo "Secondary subnet with ID $SECONDARY_SUBNET exists."
 
 # Check if security group exists
 if ! aws ec2 describe-security-groups --group-ids $SECURITY_GROUP --region $REGION > /dev/null 2>&1; then
@@ -66,7 +64,7 @@ echo "Using task definition: $TASK_DEFINITION"
 
 # Running the ECS task
 echo "Running the ECS task..."
-RUN_TASK_OUTPUT=$(aws ecs run-task --cluster $CLUSTER --launch-type FARGATE --task-definition $TASK_DEFINITION --network-configuration "awsvpcConfiguration={subnets=[$PRIMARY_SUBNET, $SECONDARY_SUBNET],securityGroups=[$SECURITY_GROUP],assignPublicIp=DISABLED}" --region $REGION)
+RUN_TASK_OUTPUT=$(aws ecs run-task --cluster $CLUSTER --launch-type FARGATE --task-definition $TASK_DEFINITION --network-configuration "awsvpcConfiguration={subnets=[$PRIMARY_SUBNET],securityGroups=[$SECURITY_GROUP],assignPublicIp=DISABLED}" --region $REGION)
 TASK_ARN=$(echo $RUN_TASK_OUTPUT | jq -r '.tasks[0].taskArn')
 echo "Task ARN: $TASK_ARN"
 
@@ -75,10 +73,14 @@ LOG_GROUP=$(echo $RUN_TASK_OUTPUT | jq -r '.tasks[0].containers[0].logConfigurat
 LOG_STREAM_PREFIX=$(echo $RUN_TASK_OUTPUT | jq -r '.tasks[0].containers[0].logConfiguration.options["awslogs-stream-prefix"]')
 
 # Get the full log stream name
-LOG_STREAM_NAME="${LOG_STREAM_PREFIX}/${TASK_ARN}"
+LOG_STREAM_NAME="${LOG_STREAM_PREFIX}/${SERVICE_NAME}/${TASK_ARN}"
 
 # Output the log stream name
 echo "Log Stream Name: $LOG_STREAM_NAME"
 
 # Output log group link
-echo "Log Group Link: https://console.aws.amazon.com/cloudwatch/home?region=$REGION#logsV2:log-groups/log-group/$LOG_GROUP" 
+echo "Log Group Link: https://console.aws.amazon.com/cloudwatch/home?region=$REGION#logsV2:log-groups/log-group/$LOG_GROUP/log-events/$LOG_STREAM_NAME" 
+
+# Output the task definition
+echo "Task Definition:"
+aws ecs describe-task-definition --task-definition $TASK_DEFINITION --region $REGION
